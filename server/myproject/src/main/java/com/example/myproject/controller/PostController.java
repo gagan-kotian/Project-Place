75437926ddf220d0bbe3160486085ffe72a166db
gagan-kotian <142842464+gagan-kotian@users.noreply.github.com>
2024-05-07 22:46:55 +0530

    Add files via upload

---
+++ server/myproject/src/main/java/com/example/myproject/controller/PostController.java
@@ -0,0 +1,46 @@
+package com.example.myproject.controller;
+
+import com.example.myproject.repository.PostRepository;
+import com.example.myproject.model.Post;
+import com.example.myproject.repository.SearchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class PostController {
+
+    @Autowired
+    PostRepository repo;
+    @Autowired
+    SearchRepository srepo;
+
+
+//    public void redirect(HttpServletResponse response)
+//    {
+//        response.sendRedirect("");
+//    }
+
+    @GetMapping("/allPosts")
+    @CrossOrigin
+    public List<Post> getAllPosts()
+    {
+        return repo.findAll();
+    }
+    // posts/java
+    @GetMapping("/posts/{text}")
+    @CrossOrigin
+    public List<Post> search(@PathVariable String text)
+    {
+        return srepo.findByText(text);
+    }
+
+    @PostMapping("/post")
+    @CrossOrigin
+    public Post addPost(@RequestBody Post post)
+    {
+        return repo.save(post);
+    }
+}
